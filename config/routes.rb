c2896c34bfd7132cb86e7d560db45770829d66d1
Sohal Singh <singhsohal575@gmail.com>
2024-01-06 15:59:45 +0530

    feat: add pwa

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,22 @@ Rails.application.routes.draw do
   get 'home/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+
+  # service worker & manifest in pwa
+  get "/service-worker.js" => "service_worker#service_worker"
+  get "/manifest.json" => "service_worker#manifest"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "home#index"
   # quiz model api
-namespace :api do
-  resources :lessons do
-    resources :quizzes, only: [:create], controller: 'quizzes' do
-      post 'generate_quiz', on: :member
+  namespace :api do
+    resources :lessons do
+      resources :quizzes, only: [:create], controller: 'quizzes' do
+        post 'generate_quiz', on: :member
+      end
     end
   end
-end
 end
